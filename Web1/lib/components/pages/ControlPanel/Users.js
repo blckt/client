c255ecea46d5f8420798f8ad4c2a6b4065ccbaed
blckt <denis.sadilo@nixsolutions.com>
2016-10-04 18:33:34 +0300

    Add project files.

---
+++ Web1/lib/components/pages/ControlPanel/Users.js
@@ -0,0 +1,19 @@
+﻿import React, { PropTypes } from 'react';
+
+class UsersView extends React.Component {
+    constructor(props) {
+      super(props);
+    }
+
+    componentWillMount() {
+
+    }
+
+    render() {
+      return (<div>
+            
+        </div>);
+    }
+}
+
+export default UsersView;
